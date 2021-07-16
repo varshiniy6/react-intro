00bb032bb16fb96ba5497c4f63b8ff3b66cbba67
varshini <varshiniyakkala@gmail.com>
2021-07-16 07:20:52 +0000

    Nested Jsx

---
+++ index.js
@@ -0,0 +1,8 @@
+//Nesting JSX Elements
+const element = (
+  <div>
+    <h1 className="greeting">Hello!</h1>
+    <p>Good to see you here.</p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
